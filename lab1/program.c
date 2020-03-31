09223bd69677f152c871ccac112f09c3f882390f
nesto123 <vofran.student@math.hr>
2020-03-31 14:45:27 +0200

    lab1_v2

--- lab1/program.c
+++ lab1/program.c
@@ -70,24 +70,18 @@ uint64_t	procjeni_velicinu_grupe()
 	uint64_t	M = 1000; //(ne preveliki da jedna petlja bude preduga,
 	          //ni premali da dohvat vremena utječe na rezultat)
 	uint64_t	SEKUNDI = 10 ;//(može i malo manje, ali barem 5);
-
-	double	t = ((double)clock())/CLOCKS_PER_SEC;
-//printf("%f\n", t);
+	time_t t1	=	time(NULL);
 	uint64_t	k = 0, i;
 
 	uint64_t	velicina_grupe = 1; //da generiraj_dobar_broj() obavi 1 iteraciju
-//printf("%f\n", t);
-	while (	(((double)clock())/CLOCKS_PER_SEC) < t + SEKUNDI	){
+	while (	time(NULL) < t1 + SEKUNDI	){
 		k++;
-		//printf("%f\n", t);
 		for (i = 0 ;	i <	M-1 ;	i++){
 			uint64_t	broj = generiraj_dobar_broj(velicina_grupe);
 			stavi_u_MS(	broj	);
 
 			//mora se osigurati da ova petlja preživi optimizaciju
 			//lako se kasnije ULAZ i IZLAZ ponovno postave na nulu
-//if( i==10) return velicina_grupe;
-//printf("---%d\n",i );
 		}
 
 	}
@@ -99,16 +93,29 @@ uint64_t polje[100];
 
 int main(int argc, char *argv[])
 {
-	uint64_t a, i, broj_ispisa	=	0,	broj;
-	clock_t c;
-	double	t;
+	uint64_t broj_ispisa	=	0,	broj;
+	//clock_t c;
+	time_t t1;
 	inicijaliziraj_generator (&p, 0);
-///		PROBLEN U OVOJ FUNKCIJI
+	ULAZ = IZLAZ = 0;
 	uint64_t	velicina_grupe	=	procjeni_velicinu_grupe();
-//ULAZ = IZLAZ = 0;
-	t	=	((double)clock())/CLOCKS_PER_SEC; // in seconds
-//return 0;
-//printf("bok %lx\n", velicina_grupe);
+	t1	=	time(NULL); // in seconds
+
+	while ( broj_ispisa < 10 ){
+
+		broj = generiraj_dobar_broj(velicina_grupe); //kod ispod
+
+		stavi_u_MS( broj	);
+
+		if( time(NULL) != t1 ) {
+//printf("%lx _ time:%ld\n", broj_ispisa+1, time(NULL));
+			printf("%lx\n" ,	uzmi_iz_MS()	);//uzmi broj iz MS i ispiši ga --uzmi_iz_MS
+			broj_ispisa++;
+			t1 = time(NULL);
+		}
+	}
+//t	=	((double)clock())/CLOCKS_PER_SEC; // in seconds
+/*
 	while ( broj_ispisa < 10 ){
 
 		broj = generiraj_dobar_broj(velicina_grupe); //kod ispod
@@ -122,20 +129,9 @@ printf("%lx _ time:%f\n", broj_ispisa+1, (((double)clock())/CLOCKS_PER_SEC));
 			t = ((double)clock())/CLOCKS_PER_SEC;
 		}
 	}
-/*
-	for ( i = 0; i < 10; i++) {
-		a	=	generiraj_dobar_broj	(&p);
-		//printf("%ld: %" PRIx64 "\n",	i ,	a	);
-
-
-		printf("%lx\n", a);
-
-	}
 */
 	obrisi_generator (&p);
 
-//t	=	((double)clock())/CLOCKS_PER_SEC; // in seconds
-//printf("%f\n",t );
 	return 0;
 }
 
